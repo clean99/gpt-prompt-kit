a7c26d5f4739891def34bf0e45d4099981b975d4
feng.xu <feng.xu@bytedance.com>
2023-03-14 20:45:11 +0800

    feat: add get code block feature

---
+++ src/textNormalization.ts
@@ -0,0 +1,6 @@
+/**
+ * A function that takes a string input and returns the normalized string.
+ * @param text
+ * @returns {string}
+ */
+export const getCodeBlock = (text: string): string | null => /```[\s\S]*?\n([\s\S]*?)\n```/.exec(text)?.[1].trim() ?? null;

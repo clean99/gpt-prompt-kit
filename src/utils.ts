4a52a73f42881e8ab7c87fae4bf6c17cd4ba1665
clean99 <xff9924@gmail.com>
2023-03-18 14:28:12 +0800

    feat: update package

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,31 @@
-import { NodeVM } from 'vm2';
-
 export async function runScript(script: string) {
   const result = {
     exports: null,
   };
-  // Create a new context for the script
-  const vm = new NodeVM({
-    console: 'inherit', // Forward the console output to the main process
-    wrapper: 'none', // Do not wrap the script in a function
-    sandbox: {
-      result,
-    }, // Provide an empty sandbox object
-    require: {
-      external: true, // Allow the 'request' external module
-      builtin: ['*'], // Allow the 'readline' built-in module
-      root: './',
-    },
-  });
+  let NodeVM;
 
-  // Run the script in the context and capture the result
-  const vmExports = await vm.run(script);
+  if (typeof window === 'undefined') {
+    NodeVM = (await import('vm2')).NodeVM;
+    // Create a new context for the script
+    const vm = new NodeVM({
+      console: 'inherit', // Forward the console output to the main process
+      wrapper: 'none', // Do not wrap the script in a function
+      sandbox: {
+        result,
+      }, // Provide an empty sandbox object
+      require: {
+        external: true, // Allow the 'request' external module
+        builtin: ['*'], // Allow the 'readline' built-in module
+        root: './',
+      },
+    });
 
-  // Return the result
-  return vmExports;
+    // Run the script in the context and capture the result
+    const vmExports = await vm.run(script);
+
+    // Return the result
+    return vmExports;
+  } else {
+    return null;
+  }
 }
